3c1b346f60d47537e769eb2f440defd59d549fbd
Khushi-Mordani <98473801+Khushi-Mordani@users.noreply.github.com>
2022-02-22 12:29:32 +0530

    Add files via upload

---
+++ op_overloading.cpp
@@ -0,0 +1,72 @@
+//Khushi Mordani
+#include<iostream>
+using namespace std;
+
+class Complex
+{
+	float real,img;
+	public:
+		Complex() //default constructor
+		{
+			real = 0;
+			img = 0;
+		}
+		Complex(int a,int b) //Parameterized constructor
+		{
+			real = a;
+			img = b;
+		}
+		void display()
+		{
+			cout<<"Real:"<<real<<" Imaginary:"<<img<<endl;
+		}
+		Complex operator +(Complex C) //addition operator overloading
+		{
+			Complex temp;
+			temp.real = real + C.real ;
+			temp.img = img + C.img;
+			return temp;
+		}
+		Complex operator -(Complex C) //subtraction operator overloading
+		{
+			Complex temp;
+			temp.real = real - C.real;
+			temp.img = img - C.img;
+			return temp;
+		}
+		Complex operator *(Complex C) //multiplication operator overloading
+		{
+			Complex temp;
+			temp.real = real * C.real;
+			temp.img = img * C.img;
+			return temp;
+		}
+		Complex operator /(Complex C) //division operator overloading
+		{
+			Complex temp;
+			temp.real = real / C.real;
+			temp.img = img / C.img;
+			return temp;
+		}
+};
+
+int main()
+{
+	Complex c1(1,2),c2(3,4),c3;
+	c1.display();
+	c2.display();
+	c3.display();
+	//c3 = c1 + c2;
+	c3 = c1.operator +(c2);
+	c3.display();
+	//c3 = c1 - c2;
+	c3 = c1.operator -(c2);
+	c3.display();
+	//c3 = c1 * c2;
+	c3 = c1.operator *(c2);
+	c3.display();
+	c3 = c1  / c2;
+	//c3 = c1.operator /(c2);
+	c3.display();
+	return 0;
+}
